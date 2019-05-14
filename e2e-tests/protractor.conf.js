ab634f6be76b5b2c0f3051a2216a1e19ab390544
George Vrettos <lukedevgr@gmail.com>
2019-05-15 01:53:24 +0300

    Add full-text filter for the phones list

---
+++ e2e-tests/protractor.conf.js
@@ -0,0 +1,25 @@
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    isVerbose: false,
+    showColors: true,
+    includeStackTrace: false,
+    defaultTimeoutInterval: 30000
+  }
+
+};
