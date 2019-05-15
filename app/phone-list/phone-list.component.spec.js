1bd5224618883df7e8fe414b82c32ef847453d5a
George Vrettos <lukedevgr@gmail.com>
2019-05-15 23:30:06 +0300

    Fetch data using an angular http service and mock this service in unit tests

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -5,14 +5,34 @@ describe('phoneList', function() {
 
     // Test the controller
     describe('PhoneListController', function() {
-        var ctrl;
+        var $httpBackend, ctrl;
 
-        beforeEach(inject(function($componentController){
+        // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+        // This allows us to inject a service and assign it to a variable with the same name
+        // as the service while avoiding a name conflict.
+        beforeEach(inject(function($componentController, _$httpBackend_) {
+            $httpBackend = _$httpBackend_;
+            // train the $httpBackend service to expect an incoming HTTP request 
+            // and what the response will be
+            $httpBackend.expectGET('phones/phones.json')
+                        .respond([
+                            {name: 'Nexus S'}, 
+                            {name: 'Motorola DROID'}
+                        ]);
+                        
             ctrl = $componentController('phoneList');
         }));
         
-        it('should create a `phones` model with 3 phones', function() {
-            expect(ctrl.phones.length).toBe(3);
+        it('should create a `phones` model with 2 phones fetched with `$http`', function() {
+            expect(ctrl.phones).toBeUndefined();
+
+            // the responses are not returned until we call flush()
+            $httpBackend.flush();
+            expect(ctrl.phones.length).toBe(2);
+            expect(ctrl.phones).toEqual([
+                {name: 'Nexus S'}, 
+                {name: 'Motorola DROID'}
+            ]);
         });
 
         it('should set a default value for the `orderProp` model', function() {
