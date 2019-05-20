ba1b7a3595971cfb1f01c894d2b1c9b9f1f804be
George Vrettos <lukedevgr@gmail.com>
2019-05-20 21:34:50 +0300

    Change the in and out animations of selected photos
    - The newly selected photo zooms in
    - The previously selected photo fades out

--- app/app.animations.js
+++ app/app.animations.js
@@ -9,36 +9,28 @@ angular
         function animateIn(element, className, done) {
             if (className !== 'selected') return;
 
-            element
-                .css({
-                    display: 'block',
-                    position: 'absolute',
-                    top: -500,
-                    left: 0
-                })
-                .animate({
-                    top: 0
-                }, 1000, done);
-            
-            return function animateInEnd(wasCanceled) {
-                if (wasCanceled) {
-                    element.stop();
-                }
-            };
+            element.css({
+                display: 'block',
+                opacity: 1,
+                position: 'absolute',
+                width: 0,
+                height: 0,
+                top: 200,
+                left: 200
+            }).animate({
+                width: 400,
+                height: 400,
+                top: 0,
+                left: 0
+            }, done);
         }
 
         function animateOut(element, className, done) {
             if (className !== 'selected') return;
 
-            element
-                .css({
-                    position: 'absolute',
-                    top: 0,
-                    left: 0
-                })
-                .animate({
-                    top: 500
-                }, 1000, done);
+            element.animate({
+                opacity: 0
+            }, done);
 
             return function animateOutEnd(wasCanceled) {
                 if (wasCanceled) {
